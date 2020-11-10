df8c7510b493ef65a24891808a93287fb1ed6efe
BuntyBru <buntybru@gmail.com>
2020-11-11 00:08:22 +0530

    email list done

--- src/js/app.js
+++ src/js/app.js
@@ -15,26 +15,26 @@ const emailList=[{
     id:ID(),
     creator:'John',
     subject:'Need to meet you',
-    content:'Hey bud, would need to meet you tomorrow',
-    tag:[],
+    content:'Hey bud, would need to meet you tomorrow, Would that be possible, Please let me know about it mate',
+    tag:['imp'],
     date:'22 Jan 2020',
     sendList:['shawn', 'michael']
 },
 {
     id:ID(),
-    creator:'John',
-    subject:'Need to meet you',
-    content:'Hey bud, would need to meet you tomorrow',
-    tag:[],
+    creator:'Doe',
+    subject:'Meeting fixed',
+    content:'Are we having a google hangout meeting or zoom ?',
+    tag:['secret'],
     date:'22 Jan 2020',
     sendList:['shawn', 'michael']
 },
 {
     id:ID(),
-    creator:'John',
-    subject:'Need to meet you',
-    content:'Hey bud, would need to meet you tomorrow',
-    tag:[],
+    creator:'Dravid',
+    subject:'Wont be able to come',
+    content:'Sorry mate, I have some work tomorrow, Can we have a meeting later on this week',
+    tag:['newsletter'],
     date:'22 Jan 2020',
     sendList:['shawn', 'michael']
 }]
@@ -63,11 +63,48 @@ class email {
                let item = document.createElement('div');
                item.classList='email-item'
                item.innerHTML = `
-               <p>${x.creator}</p>
-               <p>${x.subject}</p>
-               <p>${x.content}</p>
+               <div class='tag-holder'>
+
+               </div>
+
+               <p class='creator'>${x.creator}</p>
+               <p class='subject'>${x.subject}</p>
+               <p class='content'>${x.content}</p>
                `;
 
+               if(x.tag.length>0)
+               {
+                let tagger = item.querySelector('.tag-holder');
+                let  para = document.createElement('p');
+                   switch (x.tag[0])
+                   {
+                    case 'imp':
+                       para.classList='tag-p-imp';
+                       tagger.append(para)
+                        break;
+                    case 'book':
+                        para.classList='tag-p-book';
+                       tagger.append(para)
+                        break;
+                    case 'work':
+                            para.classList='tag-p-work';
+                           tagger.append(para)
+                            break;
+                    case 'personal':
+                        para.classList='tag-p-personal';
+                        tagger.append(para)
+                        break;
+                    case 'newsletter':
+                        para.classList='tag-p-newsletter';
+                        tagger.append(para)
+                        break;
+                    case 'secret':
+                        para.classList='tag-p-secret';
+                        tagger.append(para)
+                        break;
+                   }
+               
+               }
                emailListUI.append(item)
            })
        }
